c355c34c8f271e46adba22a1e3b8b1d796492cc9
Carlo-Laynes <carloallanlaynes@gmail.com>
2025-06-26 10:59:14 +0800

    refactor: reuse dynamic navbar in events.php

--- events.php
+++ events.php
@@ -366,30 +366,7 @@ $result = $conn->query($query);
 </head>
 <body class="d-flex flex-column min-vh-100">
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-sm">
-            <a class="navbar-brand" href="index.php">
-                <i class="bi bi-collection"></i> GalaGo Events
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">
-                            <i class="bi bi-house"></i> Home
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="events.php">
-                            <i class="bi bi-list"></i> All Events
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include 'includes/navbar.php'; ?>
 
     <div class="container-sm mt-4 flex-grow-1">
         <!-- Success/Error Messages -->
