c8cbef753075a956b12f92525188fbc8ae2cb551
Carlo-Laynes <carloallanlaynes@gmail.com>
2025-06-26 15:00:53 +0800

    ui(navbar): improved icons and logo design

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,24 +1,38 @@
 <nav class="navbar navbar-expand-lg navbar-dark gala-navbar">
-    <div class="container-sm">
-        <a class="navbar-brand" href="index.php">
-            <i class="bi bi-stars me-2"></i> GalaGo Events
-        </a>
-        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <i class="bi bi-list"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) === 'index.php' ? 'active' : ''; ?>" href="index.php">
-                        <i class="bi bi-house-door-fill"></i> Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) === 'events.php' ? 'active' : ''; ?>" href="events.php">
-                        <i class="bi bi-calendar-range-fill" style="font-size: 1rem;"></i> All Events
-                    </a>
-                </li>
-            </ul>
-        </div>
+  <div class="container-sm">
+    <!-- Logo / Brand -->
+    <a class="navbar-brand" href="index.php">
+      <i class="bi bi-clipboard2-check-fill me-2"></i> GalaGo Events
+    </a>
+
+    <!-- Mobile toggler button -->
+    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <i class="bi bi-list"></i>
+    </button>
+
+    <!-- Collapsible nav links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <!-- Home link with custom icon size -->
+        <li class="nav-item">
+          <a
+            class="nav-link <?php echo basename($_SERVER['PHP_SELF']) === 'index.php' ? 'active' : ''; ?>"
+            href="index.php"
+            style="--icon-size: 1.125rem;">
+            <i class="bi bi-house-door-fill nav-icon"></i> Home
+          </a>
+        </li>
+
+        <!-- All Events link with slightly smaller icon -->
+        <li class="nav-item">
+          <a
+            class="nav-link <?php echo basename($_SERVER['PHP_SELF']) === 'events.php' ? 'active' : ''; ?>"
+            href="events.php"
+            style="--icon-size: 1rem;">
+            <i class="bi bi-calendar3 nav-icon"></i> All Events
+          </a>
+        </li>
+      </ul>
     </div>
+  </div>
 </nav>
